4f37739c54bbccf824b9a8ea175fd8018ba9f563
Eric Prud'hommeaux <eric+github@w3.org>
2019-12-27 22:31:20 +0100

    ~ make sparql less greedy

--- src/languages/sparql.js
+++ src/languages/sparql.js
@@ -9,7 +9,7 @@ module.exports = function (hljs) {
   var ttl = hljs.getLanguage('ttl').exports;
   var KEYWORDS = {
     meta: 'base|10 prefix|10 @base|10 @prefix|10',
-    keyword: 'add all as|0 ask bind by|0 clear construct|10 copymove create data default define delete describe distinct drop exists filter from|0 graph|10 group having in|0 insert limit load minus named|10 not offset optional order reduced select|0 service silent to union using values where with|0',
+    keyword: 'add all as|0 ask bind by|0 clear construct|10 copymove create data default define delete describe distinct drop exists filter from|0 graph|10 group having in|0 insert limit load minus named|1 not offset optional order reduced select|0 service silent to union using values where with|0',
     function: 'abs asc avg bound ceil coalesce concat containsstrbefore count dayhours desc encode_for_uri floor group_concat if|0 iri isblank isiri isliteral isnumeric isuri langdatatype langmatches lcase max md5 min|0 minutes month now rand regex replace round sameterm sample seconds separator sha1 sha256 sha384 sha512 str strafter strdt strends strlang strlen strstarts struuid substr sum then timezone tz ucase uribnode uuid year',
     literal: 'true|0 false|0',
     built_in: 'a|0'
