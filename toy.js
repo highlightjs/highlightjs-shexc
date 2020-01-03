d8c47acf74ea31a7b02c3db0e0829ec9493d27a5
Eric Prud'hommeaux <eric+github@w3.org>
2020-01-03 13:03:30 +0100

    + tripleConstraint

--- toy.js
+++ toy.js
@@ -1,27 +1,32 @@
 hljs.registerLanguage("toy", function () {
   return function (hljs, options = {}) {
     const iri = { className: "iri", begin: /<[^>]*>/, endsParent: true }
-    const pname = { className: "pname", begin: /[a-z]*:[a-z]*/, endsParent: true }
+    const pname = { className: "pname", begin: /[A-Za-z]*:[A-Za-z]*/, endsParent: true }
     const string = { className: "str", begin: /'[^']*'/, endsParent: true }
-    const space = { className: "space", begin: /\s+/ }
+    const ws = { className: "ws", begin: /\s+/ }
     const error = { className: "error", begin: /./ }
     return {
       contains: [
         {
-          className: "predicate",
-          begin: /<|[a-z]/,
+          className: "tripleConstraint",
+          begin: /<|[A-Za-z]/,
           returnBegin: true,
-          contains: [iri, pname, error],
-          starts: {
-            contains: [space],
-            starts: {
-              className: "value",
-              end: /\B\b/,
-              contains: [iri, pname, string, error],
-            },
-          },
-        },
-        space,
+          contains: [
+            {
+              className: "predicate",
+              begin: /<|[A-Za-z]/,
+              returnBegin: true,
+              contains: [iri, pname],
+              starts: {
+                contains: [ws],
+                starts: {
+                  className: "value",
+                  begin: /<|[A-Za-z]|'/,
+                  contains: [iri, pname, string],
+                },
+              },
+            } ] },
+        ws,
         { className: "punct", begin: /(;|\|)/ },
       ],
     }
