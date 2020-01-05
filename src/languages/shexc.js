35492b5e5f649b9feb466df9ec38d7d0ac528a5f
Eric Prud'hommeaux <eric+github@w3.org>
2020-01-05 07:56:50 +0100

    + tripleConstraint grouping

--- src/languages/shexc.js
+++ src/languages/shexc.js
@@ -52,7 +52,7 @@ module.exports = function (hljs, opts = {}) {
 
   /** shape expressions from <http://shex.io/shex-semantics/index.html#prod-shapeExpression> 
    */
-  productions.shape = {
+  productions.shapeDefinition = {
     begin: /{/, end: /}/,
     relevance: 0
     // Add .contains (below) after constructing its contents.
@@ -113,7 +113,7 @@ module.exports = function (hljs, opts = {}) {
       contains: [common.literal, productions.IRIREF, productions.prefixedName]
     },
 
-    productions.shape,
+    productions.shapeDefinition,
   ]
   const shapeExpression_keywords = 'and or not closed abstract extends restricts iri bnode literal nonliteran'
     + ' length minlength maxlength'
@@ -129,15 +129,77 @@ module.exports = function (hljs, opts = {}) {
 
   /** shape expressions from <http://shex.io/shex-semantics/index.html#prod-unaryTripleExpr> 
    */
-  productions.tripleExpression = {
+  const ws = { className: "ws", begin: /\s+/ }
+  function eatSpace (next) {
+    return {
+      contains: [ws],
+      starts: next
+    }
+  }
+  const shapeDefinition = {}
+  const value = {
+    className: "value",
+    contains: [    {
+      className: 'name',
+      begin: '@' + common.iris_RE,
+      contains: [ common.PERCENT, common.UCHAR ],
+      relevance: 10
+    },
+    {
+      className: 'regexp',
+      begin: /\/([^\\/]|\\.)*\//,
+      contains: [
+        hljs.REGEXP_MODE,
+      ],
+    },
+    {
+      className: 'valueSet',
+      begin: '\\[',
+      end: '\\]',
+      contains: [common.literal, productions.IRIREF, productions.prefixedName]
+    }, {beginKeywords: 'LITERAL IRI'},
+shapeDefinition, productions.IRIREF, productions.prefixedName, common.literal],
+    // contains: [productions.IRIREF, productions.prefixedName, common.literal].concat(shapeExprContentModel),
+  }
+  const predicate_value = {
+    className: "predicate",
     begin: common.iris_RE,
-    end: common.EndOfDocument,
     returnBegin: true,
-    endsWithParent: true,
-    keywords: shapeExpression_keywords,
-    contains: [productions.IRIREF, productions.prefixedName].concat(shapeExprContentModel),
+    contains: [productions.IRIREF, productions.prefixedName],
+    starts: eatSpace(value),
+  }
+
+  productions.tripleConstraint = {
+    className: "shex_tripleConstraint",
+    begin: common.iris_RE,
+    // end: common.EndOfDocument,
+    returnBegin: true,
+    // endsWithParent: true,
+    // keywords: shapeExpression_keywords,
+    contains: [predicate_value],
     relevance: 0
   }
+    Object.assign(shapeDefinition, {
+      className: "shapeDefinition",
+      begin: /(?:EXTRA|CLOSED)(?:\s|<|\{)|\{/, end: /\}/,
+      returnBegin: true, returnEnd: true,
+      // excludeBegin: true, excludeEnd: true, -- moves close curley to outside <span class="value"/>
+      contains: [
+        { className: "punct", begin: /\{/ },
+        { className: "punct", begin: /\}/, endsParent: true },
+        { className: "extra",
+          begin: /EXTRA/,
+          end: /(?:EXTRA|CLOSED)(?:\s|<|\{)|\{/,
+          returnEnd: true,
+          keywords: "EXTRA",
+          start: { contains: [productions.IRIREF, productions.prefixedName] }
+        },
+        { className: "keyword", begin: /CLOSED/ },
+        productions.tripleConstraint,
+        ws,
+        { className: "punct", begin: /(;|\|)/ },
+      ]
+    })
   productions.tripleExprLabel = {
     className: 'name',
     begin: '$' + common.iris_RE,
@@ -167,8 +229,8 @@ module.exports = function (hljs, opts = {}) {
   }
 
   // Add last component in this cycle:
-  //   shape ➜ tripleExpression ➜ shapeExpression ➜ shape
-  productions.shape.contains = [productions.tripleExprLabel, productions.inclusion, productions.tripleExpression]
+  //   shape ➜ tripleConstraint ➜ shapeExpression ➜ shape
+  productions.shapeDefinition.contains = [productions.tripleExprLabel, productions.inclusion, productions.tripleConstraint]
 
   const startingProduction = opts.startingProduction || 'shexDoc'
   if (!(startingProduction in productions))
